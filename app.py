edf5d076924c3d65fe355148e4ef6b93b214d574
Dev Patel <dev.260802@gmail.com>
2023-06-29 09:45:17 +0530

    commit

---
+++ app.py
@@ -0,0 +1,101 @@
+# Importing required libraries
+from flask import Flask, render_template
+from flask import Flask, request, jsonify
+import pickle
+import os
+import json
+import pandas as pd
+import numpy as np
+current_dir = os.path.dirname(__file__)
+
+def ValuePredictor(data = pd.DataFrame):
+      model_name1='./notebook/loan_prediction_model_LR.pkl'
+      model_name2='./notebook/loan_prediction_model_DT.pkl'
+      model_name3='./notebook/loan_prediction_model_RF.pkl'
+      print(model_name1,model_name2,model_name3)
+      model_dir=os.path.join(current_dir, model_name1)
+      loaded_model1 = pickle.load(open(model_dir, 'rb'))
+      model_dir=os.path.join(current_dir, model_name2)
+      loaded_model2 = pickle.load(open(model_dir, 'rb'))
+      model_dir=os.path.join(current_dir, model_name3)
+      loaded_model3 = pickle.load(open(model_dir, 'rb'))
+ 
+      print(loaded_model1,loaded_model2,loaded_model3)
+      result = [loaded_model1.predict(data),loaded_model2.predict(data),loaded_model3.predict(data)]
+      return result
+
+
+# Flask app
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	if request.method == 'POST':	
+            name = request.form['name']
+            Gender = request.form['gender']
+            Education = request.form['education']
+            self_employed = request.form['self_employed']
+            Married = request.form['marital_status']
+            Dependents = request.form['dependents']
+            applicant_income = request.form['applicant_income']
+            CoapplicantIncome	 = request.form['coapplicant_income']
+            loan_amount = request.form['loan_amount']
+            loan_term = request.form['loan_term']
+            Credit_History = request.form['credit_history']
+            Property_Area = request.form['property_area']
+            print(name, Gender,Education,self_employed,Married,Dependents,applicant_income,CoapplicantIncome,loan_amount,loan_term,Credit_History,Property_Area)
+            schema_name = 'data/columns_set.json'
+            schema_dir= os.path.join(current_dir, schema_name)
+            with open(schema_dir, 'r') as f:
+                cols =  json.loads(f.read())
+            schema_cols=cols['data_columns']
+            # try:
+            #       col = ('Dependents_' + str(Dependents))
+            #       if col in schema_cols.keys():
+            #          print(col)
+            #          schema_cols[col]=1
+            #       else:
+            #             pass   
+            # except:
+            #       pass
+            # try:
+            #       col = ('Property_Area_' + str(property_area))
+            #       if col in schema_cols.keys():
+            #             schema_cols[col]=1
+            #       else:
+            #             pass
+            # except:pass
+            schema_cols['Gender'] = Gender
+            schema_cols['Married'] = Married
+            schema_cols['Education'] = Education
+            schema_cols['Self_Employed'] = self_employed
+            schema_cols['ApplicantIncome'] = applicant_income
+            schema_cols['CoapplicantIncome'] = CoapplicantIncome	
+            schema_cols['LoanAmount'] = loan_amount
+            schema_cols['Loan_Amount_Term'] = loan_term
+            schema_cols['Credit_History'] = Credit_History
+            schema_cols["Property_Area"]=Property_Area
+            schema_cols["Dependents"]=Dependents
+            df = pd.DataFrame(
+				data = {k: [v] for k, v in schema_cols.items()},
+				dtype = float
+			)
+            print(df.dtypes)
+            result = ValuePredictor(data = df)
+            j=0
+            for i in result:    
+                  print(i)
+                  if int(i)==1:
+                        j+=1
+            if j >= 2:prediction = 'Dear Mr/Mrs/Ms {name}, your loan is approved!'.format(name = name)
+            else:prediction = 'Sorry Mr/Mrs/Ms {name}, your loan is rejected!'.format(name = name)
+            print(prediction)
+            return render_template('prediction.html',prediction=prediction)
+            
+
+if __name__ == '__main__':
+    app.run(debug=True)
