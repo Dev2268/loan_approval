a6bffbf8fb34e006820b511a04d9dcbb27b37ea3
Dev Patel <dev.260802@gmail.com>
2023-07-30 19:17:32 +0530

    commit

---
+++ data.py
@@ -0,0 +1,111 @@
+o_c={
+    "Personal": '''
+    <table>
+            <tr>
+                <th>Name Of Bank<th>
+                <th>Intrest Rate</th>
+                <th>Procesing Fee</th>
+                <th>Loan amount</th>
+                <th>Loan Tenure</th> 
+            </tr>
+            <tr>
+                <th>HDFC<th>
+                <td>10.5"%"p.a - 21.00%p.a</td>
+                <td>2.5"%" of loan amount</td>
+                <td>Rs 50,000 - Rs 40 lakh *(Depends on Income and other factors)</td>
+                <td>12 to 60 Months</td>
+            </tr>
+            <tr>
+                <th><a href="https://sbi.co.in/web/personal-banking/loans/personal-loans/loans-to-salaried-customers-not-maintaining-salary-account-with-us">SBI<a><th>
+                <td>10.5"%"p.a - 21.00%p.a</td>
+                <td>2.5"%" of loan amount</td>
+                <td>Rs 50,000 - Rs 40 lakh *(Depends on Income and other factors)</td>
+                <td>12 to 60 Months</td>
+            </tr>
+             <tr>
+                <th><a href="https://www.icicidirect.com/loans/personal-loan">ICICI<a><th>
+                <td>10.5"%"p.a onwards</td>
+                <td>2.5"%" of loan amount</td>
+                <td>Rs 10,000 - Rs 50 lakh *(Depends on Income and other factors)</td>
+                <td>12 to 60 Months</td>
+            </tr>
+        </table>
+        ''',
+        "Home": '''
+    <table>
+            <tr>
+                <th>Name Of Bank<th>
+                <th>Intrest Rate</th>
+                <th>Procesing Fee</th>
+                <th>Loan amount</th>
+                <th>Loan Tenure</th> 
+            </tr>
+            <tr>
+                <th>ICICI<th>
+                <td>10.5"%"p.a - 21.00%p.a</td>
+                <td>2.5"%" of loan amount</td>
+                <td>Rs 50,000 - Rs 40 lakh *(Depends on Income and other factors)</td>
+                <td>12 to 60 Months</td>
+            </tr>
+            <tr>
+                <th>RBI<th>
+                <td>10.5"%"p.a - 21.00%p.a</td>
+                <td>2.5"%" of loan amount</td>
+                <td>Rs 50,000 - Rs 40 lakh *(Depends on Income and other factors)</td>
+                <td>12 to 60 Months</td>
+            </tr>
+        </table>
+        ''',
+     "Educational": '''
+    <table>
+            <tr>
+                <th>Name Of Bank<th>
+                <th>Intrest Rate</th>
+                <th>Procesing Fee</th>
+                <th>Loan amount</th>
+                <th>Loan Tenure</th> 
+            </tr>
+            <tr>
+                <th>ICICI<th>
+                <td>10.5"%"p.a - 21.00%p.a</td>
+                <td>2.5"%" of loan amount</td>
+                <td>Rs 50,000 - Rs 40 lakh *(Depends on Income and other factors)</td>
+                <td>12 to 60 Months</td>
+            </tr>
+            <tr>
+                <th>RBI<th>
+                <td>10.5"%"p.a - 21.00%p.a</td>
+                <td>2.5"%" of loan amount</td>
+                <td>Rs 50,000 - Rs 40 lakh *(Depends on Income and other factors)</td>
+                <td>12 to 60 Months</td>
+            </tr>
+        </table>
+        ''',
+         "Car": '''
+    <table>
+            <tr>
+                <th>Name Of Bank<th>
+                <th>Intrest Rate</th>
+                <th>Procesing Fee</th>
+                <th>Loan amount</th>
+                <th>Loan Tenure</th> 
+            </tr>
+            <tr>
+                <th>ICICI<th>
+                <td>10.5"%"p.a - 21.00%p.a</td>
+                <td>2.5"%" of loan amount</td>
+                <td>Rs 50,000 - Rs 40 lakh *(Depends on Income and other factors)</td>
+                <td>12 to 60 Months</td>
+            </tr>
+            <tr>
+                <th>RBI<th>
+                <td>10.5"%"p.a - 21.00%p.a</td>
+                <td>2.5"%" of loan amount</td>
+                <td>Rs 50,000 - Rs 40 lakh *(Depends on Income and other factors)</td>
+                <td>12 to 60 Months</td>
+            </tr>
+        </table>
+        '''
+}
+
+
